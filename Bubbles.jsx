5f46f9a812a4dd3543a7b1bf34afbae2f2f2c44e
Niole Nelson <niolenelson@gmail.com>
2015-09-14 17:52:48 -0400

    svg responds to data updates

--- Bubbles.jsx
+++ Bubbles.jsx
@@ -1,25 +1,52 @@
 Bubbles = React.createClass({
   propTypes: {
-    bubbleData: React.PropTypes.arrayOf(React.PropTypes.bool)
+    bubbleData: React.PropTypes.arrayOf(React.PropTypes.bool),
+    width: React.PropTypes.number,
+    height: React.PropTypes.number
   },
   componentDidMount() {
-    this.svgContainer = d3.select(React.findDOMNode(this.refs.eventbubbles)).append("svg")
-      .attr("width", 400)
-      .attr("height", 100);
+    this.buildBubbles(this.makeData(this.props.bubbleData));
 
-    console.log('this.props.bubbleData');
-    this.buildBubbles(this.props.bubbleData);
   },
-  componentWillUpdate(newProps, newState) {
-    this.buildBubbles(newProps.bubbleData);
+  componentDidUpdate() {
+    console.log('CDU');
+    this.buildBubbles(this.makeData(this.props.bubbleData));
+  },
+  makeData(data) {
+    const xScale = d3.scale.linear()
+          .domain([0,data.length])
+          .range([0,this.props.width]);
+
+    return _.map(data, (d,i) => {
+        return {i: i, cx: xScale(i), color: d ? 'green' : 'pink'};
+      });
   },
   render() {
-    return (
-      <span ref="eventbubbles"/>
-    );
+    return React.DOM.svg({
+      width: this.props.width,
+      height: this.props.height
+    });
   },
   buildBubbles(data) {
-    console.log(data);
-    console.log(this.svgContainer);
+    const svgContainer = d3.select(this.getDOMNode());
+    let circle = svgContainer.selectAll("circles")
+                  .data(data, function(d) {return d.i;})
+
+    circle
+      .enter()
+      .append("circle")
+      .attr("cx", function (d) {
+        console.log(d.cx);
+      return d.cx; })
+      .attr("cy", function (d) { return "5px"; })
+      .attr("r", function (d) { return "15px"; })
+      .style("fill", function(d) {
+        console.log(d.color);
+        return d.color;
+      });
+
+    circle.exit()
+        .remove();
+
   }
 });
--- eventbubbles.jsx
+++ eventbubbles.jsx
@@ -20,23 +20,38 @@ Tree = React.createClass({
       event.preventDefault();
       this.showEvent(event);
     }.bind(this));
-
-
+  },
+  updateState() {
+    let ind = Math.ceil(this.state.eventBubbles.length*Math.random());
+    this.setState({eventBubbles: _.map(this.state.eventBubbles, (e,i) => {
+        if (i === ind-1) {
+          return true;
+        }
+        return false;
+      })
+    });
   },
   render() {
     return (
-      <div id="event-0" className="node">
-        <div id="event-1" className="node">
-          <div id="event-2" className="node">
-            click
-            {this.showBubbles(this.state.eventBubbles)}
+      <span>
+        <button onClick={this.updateState}>update</button>
+        <div id="event-0" className="node">
+          <div id="event-1" className="node">
+            <div id="event-2" className="node">
+              click
+              {this.showBubbles(this.state.eventBubbles)}
+            </div>
           </div>
         </div>
-      </div>
+      </span>
     );
   },
   showBubbles(eventArray) {
-    return <Bubbles bubbleData={this.state.eventBubbles}/>;
+    return <Bubbles
+            width={400}
+            height={100}
+            bubbleData={this.state.eventBubbles}
+            />;
   },
   showEvent(event) {
     let currId = parseInt(event.currentTarget.id.match(/([0-9])/g)[0]);
